e3980d72f8233fefc11547e57ef38f8053421433
Maxthon Chan <xcvista@me.com>
2016-06-25 15:18:07 +0800

    Initial Commit

---
+++ xcode-register/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  xcode-register
+//
+//  Created by Maxthon Chan on 6/25/16.
+//  Copyright © 2016 DreamCity. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
